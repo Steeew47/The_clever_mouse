f10644f1f10963f52536961bbbf985a9c7347491
Steeew47 <Prkgsa111>
2018-10-24 20:40:15 +0200

    alpha v0.3

--- app/src/main/java/app/the_clever_mouse/GameView.java
+++ app/src/main/java/app/the_clever_mouse/GameView.java
@@ -10,12 +10,16 @@ public class GameView extends View {
 
     private Bitmap mouse;
 
+    public int screenHeight = getResources().getDisplayMetrics().heightPixels;
+    public int screenWidth = getResources().getDisplayMetrics().widthPixels;
+    public int mouse_size = screenWidth/5;
+
+
 
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
 
         canvas.drawBitmap(mouse,0,0,null);
     }
@@ -24,7 +28,7 @@ public class GameView extends View {
 
         super(context);
 
-        mouse = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.mouse),200,200,false);
+        mouse = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.mouse),mouse_size,mouse_size+125,false);
 
 
     }
--- app/src/main/java/app/the_clever_mouse/MainActivity.java
+++ app/src/main/java/app/the_clever_mouse/MainActivity.java
@@ -1,7 +1,14 @@
 package app.the_clever_mouse;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import app.the_clever_mouse.GameView;
+
+import android.util.DisplayMetrics;
+import android.util.Log;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +20,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         gameView = new GameView(this);
         setContentView(gameView);
+        Log.d("UWUGA",Integer.toString(gameView.mouse_size));
 
     }
 }
