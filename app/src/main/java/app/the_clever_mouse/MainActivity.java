187b130762d87a8af06518b33953c799af0ed79e
Steeew47 <Prkgsa111>
2018-10-24 18:07:38 +0200

    wersja testowa

---
+++ app/src/main/java/app/the_clever_mouse/MainActivity.java
@@ -0,0 +1,14 @@
+package app.the_clever_mouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
